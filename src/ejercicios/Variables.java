5b05911d6f75e12921aef2cbf69fdba5710854e1
Angie <angelicazamudiocamargo@gmail.com>
2022-09-26 21:13:48 -0500

    Initial commit

---
+++ src/ejercicios/Variables.java
@@ -0,0 +1,30 @@
+package ejercicios;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int entero = 3;
+        short smallNumber = 3;
+        byte chiquito = 6;
+
+        float decimal = 2.5f;
+        double pi = 3.1416d;
+
+        String texto = "Bla bla bla";
+        char character = 'c';
+        boolean boleano = true;
+
+        System.out.println(entero);
+        System.out.println(smallNumber);
+        System.out.println(chiquito);
+        System.out.println(decimal);
+        System.out.println(pi);
+        System.out.println(texto);
+        System.out.println(character);
+        System.out.println(boleano);
+    }
+
+
+
+}
